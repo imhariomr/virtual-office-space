f38a03bfbf7d0bc2779e18200629700b2392ec37
imhariomr <imhariomr@gmail.com>
2025-08-22 00:08:07 +0530

    initalize

---
+++ packages/components/src/floor.tsx
@@ -0,0 +1,16 @@
+// "use client"; 
+// import { useTexture } from "@react-three/drei";
+
+// export default function Floor() {
+//   const texture = useTexture("/floor.jpg"); 
+
+//   return (
+//     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+//       {/* flat surface */}
+//       <planeGeometry args={[20, 20]} />
+
+//       {/* texture applied */}
+//       <meshStandardMaterial map={texture} />
+//     </mesh>
+//   );
+// }
